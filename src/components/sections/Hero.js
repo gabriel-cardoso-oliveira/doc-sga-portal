e1a2d75041f31a439a11c144aee97c33c9ecc3b5
Gabriel Cardoso <gabrielfjggyn@gmail.com>
2021-03-23 18:40:55 -0300

    First commit

---
+++ src/components/sections/Hero.js
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import { SectionProps } from '../../utils/SectionProps';
+import ButtonGroup from '../elements/ButtonGroup';
+import Button from '../elements/Button';
+// import Image from '../elements/Image';
+import Modal from '../elements/Modal';
+
+const propTypes = {
+  ...SectionProps.types
+}
+
+const defaultProps = {
+  ...SectionProps.defaults
+}
+
+const Hero = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  ...props
+}) => {
+
+  const [videoModalActive, setVideomodalactive] = useState(false);
+
+  // const openModal = (e) => {
+  //   e.preventDefault();
+  //   setVideomodalactive(true);
+  // }
+
+  const closeModal = (e) => {
+    e.preventDefault();
+    setVideomodalactive(false);
+  }   
+
+  const outerClasses = classNames(
+    'hero section center-content',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'hero-inner section-inner',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container-sm">
+        <div className={innerClasses}>
+          <div className="hero-content">
+            <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
+              Documentação do <span className="text-color-primary">SGA Portal</span>
+            </h1>
+            <div className="container-xs">
+              <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
+                Nesse guia iremos mostrar o passo a passo para preparar o Ambiente SGA Portal em sistemas Windows. Você irá aprender também como executar. Além disso, falaremos também sobre os erros mais comuns relacionados e como solucioná-los.
+              </p>
+              <div className="reveal-from-bottom" data-reveal-delay="600">
+                <ButtonGroup>
+                  <Button
+                    tag="a"
+                    color="primary"
+                    wideMobile
+                    href="https://drive.google.com/file/d/1i81HymlJ49mqK3CDu4lfyNj_2IhW1kcz/view?usp=sharing"
+                    target="_blank"
+                  >
+                    Download Arquivos
+                  </Button>
+                  <Button
+                    tag="a"
+                    color="dark"
+                    wideMobile
+                    href="https://drive.google.com/file/d/1qzKC_el7y90T9INuHvPLgqOalCSuhQ56/view?usp=sharing"
+                    target="_blank"
+                  >
+                    Download Programas
+                  </Button>
+                </ButtonGroup>
+              </div>
+            </div>
+          </div>
+          {/* <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
+            <a
+              data-video="https://player.vimeo.com/video/174002812"
+              href="#0"
+              aria-controls="video-modal"
+              onClick={openModal}
+            >
+              <Image
+                className="has-shadow"
+                src={require('./../../assets/images/video-placeholder.jpg')}
+                alt="Hero"
+                width={896}
+                height={504} />
+            </a>
+          </div> */}
+          <Modal
+            id="video-modal"
+            show={videoModalActive}
+            handleClose={closeModal}
+            video="https://player.vimeo.com/video/174002812"
+            videoTag="iframe" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+Hero.propTypes = propTypes;
+Hero.defaultProps = defaultProps;
+
+export default Hero;
