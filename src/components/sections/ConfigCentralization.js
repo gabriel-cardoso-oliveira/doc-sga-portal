de43137ad2042c6dad0470f1ff2afbd9c23cb427
Gabriel Cardoso <gabrielfjggyn@gmail.com>
2021-04-05 18:35:44 -0300

    Added instructions 'Scheduler'

--- src/components/sections/ConfigCentralization.js
+++ src/components/sections/ConfigCentralization.js
@@ -74,16 +74,20 @@ const FeaturesSplit = ({
                   Configurando a zincronização
                 </h3>
                 <p className="m-0">
-                  - Clique <a href="https://drive.google.com/file/d/1SSn6sJ2nDTNmrmphVulIIr4y9WvwyCG6/view?usp=sharing" target="_blank">aqui</a> para baixar o "<strong>System Scheduler</strong>" (programa para configurar a sincronização);
+                  - Clique <strong><a href="https://drive.google.com/file/d/1vdlH-6467C87bD2r9QVsSfsYZWLKDQbi/view?usp=sharing" target="_blank">aqui</a></strong> para baixar o "<strong>System Scheduler</strong>" (<code>programa que configura e ativa o bat da sincronização dos bancos de dados das unidades</code>);
                 </p>
                 <p className="m-0">
-                  - Ele acontece porque o "<strong>Visual C++ Redistributable for Visual Studio 2012</strong>" não está instalado no PC;
+                  - Ao finalizar o download, execute o arquivo "<strong>SystemScheduler</strong>", na tela que abrir, escolha o diretório da extração em "<strong>C:\digital\data</strong>" e clique no botão "<strong>Extract</strong>";
                 </p>
                 <p className="m-0">
-                  - Para resolver esse erro acessa esté link, e faça o download do executável "<strong>VSU_4\vcredist_x64.exe</strong>" ou "<strong>VSU_4\vcredist_x86.exe</strong>", conforme a versão do Windows;
+                  - Ao finalizar a extração, acesse a pasta "<strong>C:\digital\data\SystemScheduler</strong>", execute e crie um atalho do executável "<strong>WScheduler.exe</strong>" (<code>selecione o executável e clique com o botão direito do mouse, em seguida clique na opção "<strong>Criar atalho</strong>"</code>);
                 </p>
                 <p className="m-0">
-                  Ao finalizar o download, execute o instalado, após a instalação "<strong>Reinicie o PC</strong>" e verifique se o erro persiste.
+                  - Mova esse "Atalho" para a pasta "<strong>inicializar</strong>" do Windows.
+                </p>
+                <br/>
+                <p className="m-0">
+                  <strong>Obs:</strong> Para acessar a pasta "<strong>inicializar</strong>" do Windows. Na barra de endereço da pasta, digite "<strong>inicializar</strong>" e tecle "<strong>Enter</strong>".
                 </p>
               </div>
               <div className={
@@ -93,7 +97,7 @@ const FeaturesSplit = ({
                 )}
                 data-reveal-container=".split-item">
                 <Image
-                  src={require('./../../assets/images/erro01.png')}
+                  src={require('./../../assets/images/centralizacao01.png')}
                   alt="Features split 01"
                   width={528}
                   height={396} />
