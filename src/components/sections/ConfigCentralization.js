bb06a9765a61eaf1d8260389565af23d32852cf9
Gabriel Cardoso <gabrielfjggyn@gmail.com>
2021-04-05 21:15:33 -0300

    Added instruction 'Ativando centralização'

--- src/components/sections/ConfigCentralization.js
+++ src/components/sections/ConfigCentralization.js
@@ -171,6 +171,38 @@ const FeaturesSplit = ({
               </div>
             </div>
 
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  3.1 Sistema
+                </div>
+                <h3 className="mt-0 mb-12">
+                  Ativando Centralização Unidade
+                </h3>
+                <p className="m-0">
+                  - Na página "<strong>Unidades</strong>";
+                </p>
+                <p className="m-0">
+                  - Na unidade cadastrada anteriomente, clique no botão "<strong>Editar</strong>";
+                </p>
+                <p className="m-0">
+                  - Na tela que abrir, clique no botão "<strong>Ativar centralização</strong>".
+                </p>
+              </div>
+              <div className={
+                classNames(
+                  'split-item-image center-content-mobile reveal-from-bottom',
+                  imageFill && 'split-item-image-fill'
+                )}
+                data-reveal-container=".split-item">
+                <Image
+                  src={require('./../../assets/images/centralizacao04.png')}
+                  alt="Features split 01"
+                  width={528}
+                  height={396} />
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
